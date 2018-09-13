e279188fbfb03f81f294ae376dd81032fb177788
NewProgrammerBelstu <Ptashnik97@mail.ru>
2018-09-13 16:39:26 +0300

    Добавил в группу студентку Дашу

--- git/Lab1/Lab1/Program.cs
+++ git/Lab1/Lab1/Program.cs
@@ -15,7 +15,7 @@ namespace Lab1
             Student Student3 = new Student(6, 6, 6, "Artem");
             Student Student4 = new Student(7, 7, 7, "Sasha");
             Student Student5 = new Student(8, 8, 8, "Pasha");
-
+            Student Student6 = new Student(9, 9, 9, "Dasha");
             List<Object> Group = new List<object>();
             Group.Add(Student1); Group.Add(Student2); Group.Add(Student3); Group.Add(Student4);
             double averageGroupScore = 0;
