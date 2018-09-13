2a58ee815ac92cb9549d3b4827c327959bac4efd
NewProgrammerBelstu <Ptashnik97@mail.ru>
2018-09-13 16:28:35 +0300

    Добавил лабу по вычислению среднего балла

---
+++ git/Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Student1 = new Student(5, 5, 5, "Egor");
+            Student Student2 = new Student(4, 4, 4, "Anton");
+            Student Student3 = new Student(6, 6, 6, "Artem");
+            Student Student4 = new Student(7, 7, 7, "Sasha");
+            List<Object> Group = new List<object>();
+            Group.Add(Student1); Group.Add(Student2); Group.Add(Student3); Group.Add(Student4);
+            double averageGroupScore = 0;
+            foreach (Student i in Group)
+            {
+                i.ShowInfo();
+                averageGroupScore = averageGroupScore + i.AverageScore();
+            }
+            Console.WriteLine("Average group score: " + (averageGroupScore/Group.Count));
+        }
+    }
+}
