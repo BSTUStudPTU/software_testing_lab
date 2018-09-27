dfe1f16e47d941ecf4f4e1b39dbb2c776fff9c82
NewProgrammerBelstu <Ptashnik97@mail.ru>
2018-09-27 21:47:46 +0300

    Created triangle inequality method

---
+++ Unit_Testing/Lab3/Lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        public static bool TriangleCheck(double firstSide, double secondSide, double thirdSide)
+        {
+            if ((firstSide + secondSide > thirdSide) && (secondSide + thirdSide > firstSide) && (firstSide + thirdSide > secondSide))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
