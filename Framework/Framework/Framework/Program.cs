263545049b15e1bd77b6aadf6f4ba5eb46d7a793
Harseko <nika.9@mail.ru>
2019-01-10 20:33:28 +0300

    Delete Program.cs
    Removed Program.cs (reverted from commit aef74538cf64603c23495c94e81fd0648b75af0a)

---
+++ Framework/Framework/Framework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Framework.Tests.Tests tests = new Framework.Tests.Tests();
+            tests.GoToRegistrationPage();
+        }
+    }
+}
